e03289d763d887451f808e966b08382ffc43a612
ktrichin <rikarikamika2002@gmail.com>
2024-12-31 18:23:23 +0100

    dd

---
+++ js/page.js
@@ -0,0 +1,6 @@
+const atuhdden = document.getElementById("atuhaiden")
+const showheedden = document.getElementById("button")
+
+showheedden?.addEventListener("click", () => {
+    atuhdden.classList.remove("haidden");
+})
