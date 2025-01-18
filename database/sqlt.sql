4e939c46526c209e11a3fbcd4adcbc634f420fde
ktrichin <rikarikamika2002@gmail.com>
2025-01-18 18:33:21 +0100

    dd*

--- database/sqlt.sql
+++ database/sqlt.sql
@@ -6,22 +6,3 @@
             password TEXT NOT NULL,
             created_at DATETIME DEFAULT CURRENT_TIMESTAMP
         );
-
-    CREATE TABLE
-        IF NOT EXISTS sessions (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id INTEGER NOT NULL,
-            token TEXT NOT NULL UNIQUE,
-            expires_at DATETIME NOT NULL,
-            FOREIGN KEY (user_id) REFERENCES users (id)
-        );
-
-    CREATE TABLE
-        IF NOT EXISTS posts  (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id INTEGER NOT NULL,
-            title TEXT NOT NULL,
-            content TEXT NOT NULL,
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-            
-        )
