3023072a1ef0c3ca65f72eb1dc4143977a938096
ktrichin <rikarikamika2002@gmail.com>
2025-02-07 18:47:23 +0100

    v

--- js/page.js
+++ js/page.js
@@ -61,36 +61,36 @@ function togglePage() {
   }
 
 
-page heders
+// page heders
 
-function updateClock() {
-    const hours = document.getElementById('hrs');
-    const minutes = document.getElementById('mins');
-    const seconds = document.getElementById('secs');
+// function updateClock() {
+//     const hours = document.getElementById('hrs');
+//     const minutes = document.getElementById('mins');
+//     const seconds = document.getElementById('secs');
 
-    const hhCircle = document.getElementById('hh');
-    const mmCircle = document.getElementById('mm');
-    const ssCircle = document.getElementById('ss');
+//     const hhCircle = document.getElementById('hh');
+//     const mmCircle = document.getElementById('mm');
+//     const ssCircle = document.getElementById('ss');
 
-    const now = new Date();
-    let hrs = now.getHours();
-    let mins = now.getMinutes();
-    let secs = now.getSeconds();
+//     const now = new Date();
+//     let hrs = now.getHours();
+//     let mins = now.getMinutes();
+//     let secs = now.getSeconds();
 
-    hrs = hrs % 12 || 12;
+//     hrs = hrs % 12 || 12;
 
-    hours.innerText = hrs.toString().padStart(2, '0');
-    minutes.innerText = mins.toString().padStart(2, '0');
-    seconds.innerText = secs.toString().padStart(2, '0');
+//     hours.innerText = hrs.toString().padStart(2, '0');
+//     minutes.innerText = mins.toString().padStart(2, '0');
+//     seconds.innerText = secs.toString().padStart(2, '0');
 
-    const circumference = 251.2;
+//     const circumference = 251.2;
 
-    hhCircle.style.strokeDashoffset = circumference - (circumference * hrs) / 12;
-    mmCircle.style.strokeDashoffset = circumference - (circumference * mins) / 60;
-    ssCircle.style.strokeDashoffset = circumference - (circumference * secs) / 60;
-}
+//     hhCircle.style.strokeDashoffset = circumference - (circumference * hrs) / 12;
+//     mmCircle.style.strokeDashoffset = circumference - (circumference * mins) / 60;
+//     ssCircle.style.strokeDashoffset = circumference - (circumference * secs) / 60;
+// }
 
-setInterval(updateClock, 1000);
-updateClock();
-// mergen
-//dd
+// setInterval(updateClock, 1000);
+// updateClock();
+// // mergen
+// //dd
